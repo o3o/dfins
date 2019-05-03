b85306565f9ffce088872fda4737e9ae5e2ceb78
Orfeo Da Vià <orfeo.davia@microline.eu>
2019-05-03 10:41:25 +0200

    Refactoring

--- examples/readdm/app.d
+++ examples/readdm/app.d
@@ -5,13 +5,15 @@ import dfins.channel;
 
 void main(string[] args) {
    IChannel chan  = createUdpChannel("192.168.221.22", 2000);
-   FinsClient f = new FinsClient(chan);
-   f.sa1 = 22;
-   f.sna = 0;
-   f.da1 = 2;
-   f.dna = 0;
+
+   Header h = header(22);
+   FinsClient f = new FinsClient(chan, h);
 
    ubyte[] d0 = f.readArea(MemoryArea.D_WORD, 0, 1);
-   writefln("len %s", d0.length);
+   writefln("DM000 len %s", d0.length);
    writefln("%( %s %)", d0);
+
+   ubyte[] d1 = f.readArea(MemoryArea.D_WORD, 1, 1);
+   writefln("DM001 len %s", d1.length);
+   writefln("%( %s %)", d1);
 }
--- src/dfins/fins.d
+++ src/dfins/fins.d
@@ -3,16 +3,16 @@ module dfins.fins;
 import dfins.channel;
 
 enum MemoryArea : ubyte {
-   CIO_BIT  = 0x30,
-   W_BIT    = 0x31,
-   H_BIT    = 0x32,
-   A_BIT    = 0x33,
-   D_BIT    = 0x02,
+   CIO_BIT = 0x30,
+   W_BIT = 0x31,
+   H_BIT = 0x32,
+   A_BIT = 0x33,
+   D_BIT = 0x02,
    CIO_WORD = 0xB0,
-   W_WORD   = 0xB1,
-   H_WORD   = 0xB2,
-   A_WORD   = 0xB3,
-   D_WORD   = 0x82
+   W_WORD = 0xB1,
+   H_WORD = 0xB2,
+   A_WORD = 0xB3,
+   D_WORD = 0x82
 }
 
 enum ErrorCodes {
@@ -40,31 +40,44 @@ enum ErrorCodes {
    finsErrCannotWrite = 0x00010009
 }
 
-struct CommandData {
+struct Header {
    /**
     * Information Control Field, set to 0x80
     */
    ubyte icf = 0x80;
-   ubyte rsv;//RESERVED, SET TO 0x00
-   ubyte gct = 0x02;//GATEWAY COUNT, SET TO 0x02
-   ubyte dna;//DESTINATION NETWORK, 0x01 IF THERE ARE NOT NETWORK INTERMEDIARIES
-   ubyte da1;//DESTINATION NODE NUMBER, IF SET TO DEFAULT THIS IS THE SUBNET BYTE OF THE IP OF THE PLC (EX. 192.168.0.1 -> 0x01)
-   ubyte da2;//DESTINATION UNIT NUMBER, THE UNIT NUMBER, SEE THE HW CONIFG OF PLC, GENERALLY 0x00
-   ubyte sna;//SOURCE NETWORK, GENERALLY 0x01
-   ubyte sa1;//SOURCE NODE NUMBER, LIKE THE DESTINATION NODE NUMBER, YOU COULD SET A FIXED NUMBER INTO PLC CONFIG
-   ubyte sa2;//SOURCE UNIT NUMBER, LIKE THE DESTINATION UNIT NUMBER
-   ubyte sid;//COUNTER FOR THE RESEND, GENERALLY 0x00
-   ubyte mr= 0x01;
-   ubyte sr= 0x01;
-   ubyte[] text;
+   //ubyte rsv;//reserved, set to 0x00
+   //ubyte gct = 0x02;//gateway count, set to 0x02
+   ubyte dna; //destination network, 0x01 if there are not network intermediaries
+   ubyte da1; //destination node number, if set to default this is the subnet byte of the ip of the plc (ex. 192.168.0.1 -> 0x01)
+   ubyte da2; //destination unit number, the unit number, see the hw conifg of plc, generally 0x00
+   ubyte sna; //source network, generally 0x01
+   ubyte sa1 = 0x02; //source node number, like the destination node number, you could set a fixed number into plc config
+   ubyte sa2; //source unit number, like the destination unit number
+   ubyte sid; //counter for the resend, generally 0x00
+   ubyte mainCmdCode; // command code high byte
+   ubyte subCmdCode; // command code low byte
 }
 
-ubyte[] toBytes(CommandData data) {
+/**
+ * Convenience function for creating an `Header` with subnet data (`da1`)
+ */
+Header header(ubyte subnet) {
+   Header h;
+   h.da1 = subnet;
+   return h;
+}
+
+/**
+ *  Convert an `Header` to array of bytes.
+ */
+ubyte[] toBytes(Header data) {
    //ubyte[] b = new ubyte[](12 + data.text.length);
-   ubyte[] b ;
+   enum RSV = 0x0;
+   enum GCT = 0x02;
+   ubyte[] b;
    b ~= data.icf;
-   b ~= data.rsv;
-   b ~= data.gct;
+   b ~= RSV;
+   b ~= GCT;
    b ~= data.dna;
    b ~= data.da1;
    b ~= data.da2;
@@ -72,75 +85,68 @@ ubyte[] toBytes(CommandData data) {
    b ~= data.sa1;
    b ~= data.sa2;
    b ~= data.sid;
-   b ~= data.mr;
-   b ~= data.sr;
-   foreach (elem; data.text) {
-      b ~= elem;
-   }
+   b ~= data.mainCmdCode;
+   b ~= data.subCmdCode;
    return b;
 }
 
 unittest {
-   CommandData data;
+   Header data;
    data.dna = 0;
    data.da1 = 0x16;
    data.da2 = 0;
    data.sna = 0;
    data.sa1 = 0x02;
    data.sa2 = 0;
-   ubyte[] exp = [0x80, 0x00, 0x02,
-      0x00, 0x16, 0x0,
-      0x00, 0x02, 0x0,
-      0x0,0x01,0x1
-   ];
+   ubyte[] exp = [0x80, 0x00, 0x02, 0x00, 0x16, 0x0, 0x00, 0x02, 0x0, 0x0, 0x01, 0x01];
    auto b = data.toBytes;
    import std.stdio;
+
    writeln(b.length);
 
    assert(b.length == 12);
    import std.conv;
-for (int i = 0; i < 12; ++i) {
-   assert(b[i] == exp[i], i.to!string());
-}
+
+   for (int i = 0; i < 10; ++i) {
+      assert(b[i] == exp[i], i.to!string());
+   }
    //assert(data.toBytes == exp);
 }
 
+/**
+ * Converts an array of bytes to `Header`
+ */
+Header toHeader(ubyte[] blob)
+in {
+   assert(blob.length > 11, "Blob too short (less than 12)");
+}
+do {
+   Header h;
+   h.icf = blob[0];
+   h.dna = blob[3];
+   h.da1 = blob[4];
+   h.da2 = blob[5];
+   h.sna = blob[6];
+   h.sa1 = blob[7];
+   h.sa2 = blob[8];
+   h.sid = blob[9];
+   h.mainCmdCode = blob[10];
+   h.subCmdCode = blob[11];
+   return h;
+}
 
 struct ResponseData {
-   public ubyte ICF;
-   public ubyte RSV;
-   public ubyte GCT;
-   public ubyte DNA;
-   public ubyte DA1;
-   public ubyte DA2;
-   public ubyte SNA;
-   public ubyte SA1;
-   public ubyte SA2;
-   public ubyte SID;
-   public ubyte MR;
-   public ubyte SR;
-   public ubyte rspCodeH;
-   public ubyte rspCodeL;
-   public ubyte[] text;
+   Header header;
+   ubyte mainRspCode;
+   ubyte subRspCode;
+   ubyte[] text;
 }
 
 ResponseData toResponse(ubyte[] data) {
    ResponseData resp;
-   resp.text = new ubyte[](data.length - 14);
-   resp.ICF = data[0];
-   resp.RSV = data[1];
-   resp.GCT = data[2];
-   resp.DNA = data[3];
-   resp.DA1 = data[4];
-   resp.DA2 = data[5];
-   resp.SNA = data[6];
-   resp.SA1 = data[7];
-   resp.SA2 = data[8];
-   resp.SID = data[9];
-   resp.MR = data[10];
-   resp.SR = data[11];
-   resp.rspCodeH = data[12];
-   resp.rspCodeL = data[13];
+   resp.header = data.toHeader;
+   resp.mainRspCode = data[12];
+   resp.subRspCode = data[13];
    for (int i = 0; i < data.length - 14; i++) {
       resp.text ~= data[14 + i];
    }
@@ -148,89 +154,13 @@ ResponseData toResponse(ubyte[] data) {
 }
 
 class FinsClient {
-   /+
-   /**
-    * Instantiates a new FinsClient
-    *
-    * Params:
-    *  DNA = Destination Network, look at the PLC configuration
-    *  DA1 = Destination Node Number, if set to default this is the last IP byte (subnet) of the plc. Example: 192.168.0.1 -> 0x01. You can set a fixed number into PLC configuration
-    *  DA2 = Destination Unit Number, the unit number, see the hw conifg of plc, generally 0x00.
-    *  SNA = Source Network, if in the same network of PLC, it is like DNA.
-    *  SA1 = Source Node Number, like the destination node number. You can set a fixed number into PLC configuration
-    *  SA2 = Source Unit Number, like the destination unit number.
-    *
-    */
-   this(byte DNA = 0x01, byte DA1 = 0x01, byte DA2 = 0x00, byte SNA = 0x01, byte SA1 = 0x01, byte SA2 = 0x01) {
-      //destination properties
-      DestinationNetworkAddress = DNA;   //DNA
-      DestinationNodeNumber = DA1;       //DA1
-      DestinationUnitNumber = DA2;       //DA2
-
-      //source properties
-      SourceNetworkAddress = SNA;        //SNA
-      SourceNodeNumber = SA1;            //SA1
-      SourceUnitNumber = SA2;            //SA2
-   }
-   +/
-
    private IChannel channel;
-   this(IChannel channel) {
+   private Header header;
+   this(IChannel channel, Header header) {
       assert(channel !is null);
       this.channel = channel;
+      this.header = header;
    }
-
-   private ubyte _dna = 0x01;
-   /**
-    * Destination Network, look at the PLC configuration
-    */
-   ubyte dna() { return _dna; }
-   void dna(ubyte value) { _dna = value; }
-
-
-   private ubyte _da1 = 0x01;
-   /**
-    * Destination Node Number, if set to default this is the last IP byte (subnet) of the plc.
-    *
-    * Examples:
-    * --------------------
-    * 192.168.0.1 -> 0x01.
-    * --------------------
-    *
-    * You can set a fixed number into PLC configuration
-    */
-   ubyte da1() { return _da1; }
-   void da1(ubyte value) { _da1 = value; }
-
-   private ubyte _da2 = 0x00;
-   /**
-    * Destination Unit Number, the unit number, see the hw conifg of plc, generally 0x00.
-    */
-   ubyte da2() { return _da2; }
-   void da2(ubyte value) { _da2 = value; }
-
-   private ubyte _sna = 0x01;
-
-   /**
-    * Source Network, if in the same network of PLC, it is like DNA.
-    */
-   ubyte sna() { return _sna; }
-   void sna(ubyte value) { _sna = value; }
-
-   private ubyte _sa1 = 0x01;
-   /**
-    * Source Node Number, like the destination node number. You can set a fixed number into PLC configuration
-    */
-   ubyte sa1() { return _sa1; }
-   void sa1(ubyte value) { _sa1 = value; }
-
-   private ubyte _sa2 = 0x01;
-   /**
-    * Source Unit Number, like the destination unit number.
-    */
-   ubyte sa2() { return _sa2; }
-   void sa2(ubyte value) { _sa2 = value; }
-
 
    /**
     * Write an Omron PLC area: the area must be defined as CJ like area
@@ -240,9 +170,6 @@ class FinsClient {
     *  start = The start offset for the write process.
     *  size = The size of the area to write. IMPORTANT: The size is expressed in WORD (2 byte)
     *  buffer = The byte array buffer which will be write in the PLC.
-    *
-    *
-    * Returns: The status integer
     *
     */
    void writeArea(MemoryArea area, ushort start, ushort size, ubyte[] buffer) {
@@ -264,7 +191,7 @@ class FinsClient {
       }
 
       //sending the fins command and storing the response
-      sendFinsCommand(0x01, 0x02, text);
+      //sendFinsCommand(0x01, 0x02, text);
    }
 
    /**
@@ -282,50 +209,36 @@ class FinsClient {
     */
    ubyte[] readArea(MemoryArea area, ushort start, ushort size) {
       import std.stdio;
-      ubyte[] text = new ubyte[](6);
-      writefln("area %s", area);
+
+      ubyte[] cmdBlock;
 
       //memory area code
-      text ~= cast(ubyte)area;
+      cmdBlock ~= cast(ubyte)area;
 
       //beginning address
-      text ~= cast(ubyte)(start >> 8);
-      text ~= cast(ubyte)start;
-      text ~= 0x00;
+      cmdBlock ~= cast(ubyte)(start >> 8);
+      cmdBlock ~= cast(ubyte)start;
+      cmdBlock ~= 0x00;
 
       //number of items
-      text ~= cast(ubyte)(size >> 8);
-      text ~= cast(ubyte)size;
+      cmdBlock ~= cast(ubyte)(size >> 8);
+      cmdBlock ~= cast(ubyte)size;
 
-      writefln("%( %s %)", text);
-      return sendFinsCommand(0x01, 0x01, text);
+      return sendFinsCommand(0x01, 0x01, cmdBlock);
    }
 
-   private ubyte[] sendFinsCommand(ubyte MR, ubyte SR, ubyte[] comText) {
-      CommandData commandFrame;
-      commandFrame.icf = 0x80;
-      commandFrame.rsv = 0x00;
-      commandFrame.gct = 0x02;
-      commandFrame.dna = _dna;
-      commandFrame.da1 = _da1;
-      commandFrame.da2 = _da2;
-      commandFrame.sna = _sna;
-      commandFrame.sa1 = _sa1;
-      commandFrame.sa2 = _sa2;
-      commandFrame.sid = 0x00;
-      commandFrame.mr = MR;
-      commandFrame.sr = SR;
-      commandFrame.text = comText;
+   private ubyte[] sendFinsCommand(ubyte cmdH, ubyte cmdL, ubyte[] comText) {
+      header.mainCmdCode = cmdH;
+      header.subCmdCode = cmdL;
 
-      // Frame send via UDP
-      ubyte[] sendFrame = commandFrame.toBytes();
+      ubyte[] sendFrame = header.toBytes() ~ comText;
       ubyte[] receiveFrame = channel.send(sendFrame);
 
       //Frame deconstruct
       ResponseData response = receiveFrame.toResponse();
       //response execution code
-      if (response.rspCodeH != 0) {
-         switch (response.rspCodeH) {
+      if (response.mainRspCode != 0) {
+         switch (response.mainRspCode) {
             case 1:
                //currentStatus = (int)FinsProtocol.ErrorCodes.finsErrLocalNode;
                break;
